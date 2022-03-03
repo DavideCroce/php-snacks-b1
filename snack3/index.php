d5b51ead1cd195446d5326a8ec567afe95d46c7a
IskahnX <davidecroce10@gmail.com>
2022-03-03 16:13:47 +0100

    snack3 done

--- snack3/index.php
+++ snack3/index.php
@@ -8,4 +8,13 @@
 // Snack 3
 // Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.
 
-$numb = [];
+$numbs = [];
+
+while (count($numbs) !=  15) {
+    $r_numbs = rand(1, 100);
+    if (!in_array($r_numbs, $numbs)) {
+        $numbs = $r_numbs;
+    }
+};
+
+var_dump($numbs);
